a5a0a38c3171c1dc878c1a2ab30bb7bcca170d25
Juan Mallarino <jmallarino@hugeinc.com>
2022-02-03 10:36:20 -0500

    [jmallarino] Selenium profile test

---
+++ test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+service=Service(ChromeDriverManager().install())
+
+options = webdriver.ChromeOptions()
+options.add_argument(r'user-data-dir=/Users/jmallarino/Library/Application Support/Google/Chrome')
+options.add_argument(r'profile-directory=Default')
+
+cls.driver = webdriver.Chrome(
+    service=service,
+    options=options
+)
+
+# cls.get("https://google.com/")
+cls.get("http://stackoverflow.com/")
+
+body = cls.find_element_by_tag_name("body")
+body.send_keys(Keys.CONTROL + 't')
+
+cls.close()
+
+print(body)
